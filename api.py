03009013b27274c2f9d54a393b554f60d7544ee4
SeungHyeon <namsh1125@naver.com>
2023-12-17 00:52:46 +0900

    [Refactor] api.py 리팩토링

--- api.py
+++ api.py
@@ -3,12 +3,36 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import tensorflow as tf
 import numpy as np
-from tensorflow import keras
 import pickle
 from keras.preprocessing import sequence
 from embedding import to_index_array, padding, decompose_string
 
+
+class Item(BaseModel):
+    text: str
+
+
+class ProfanityChecker:
+    def __init__(self, model_path, dict_path, max_len):
+        self.model = tf.keras.models.load_model(model_path)
+        with open(dict_path, 'rb') as f:
+            self.jamodict = pickle.load(f)
+        self.max_len = max_len
+
+    def encode_review(self, text):
+        text = decompose_string(text)
+        text = to_index_array(text, self.jamodict)
+        return padding(text, self.max_len)
+
+    def predict(self, text):
+        indices = self.encode_review(text)
+        indices = np.array([indices])
+        y_prob = self.model.predict(indices)
+        return bool(y_prob.argmax(axis=-1) == 1)
+
+
 app = FastAPI()
+checker = ProfanityChecker('models/latest-yok-detect-model.h5', 'jamo.pydict', 681)
 
 origins = [
     "http://localhost",
@@ -23,44 +47,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-MAX_LEN = 681
-
-with open('jamo.pydict', 'rb') as f:
-    jamodict = pickle.load(f)
-
-
-def encode_review(text):
-    text = decompose_string(text)
-    text = to_index_array(text, jamodict)
-    text = padding(text, MAX_LEN)
-    
-class Item(BaseModel):
-  text: str
-
-
-def predict(text):
-  model = tf.keras.models.load_model('models/latest-yok-detect-model.h5')
-  
-  indices = encode_review(text)
-  indices = np.array([indices])
-  
-  y_prob = model.predict(indices)
-  result = y_prob.argmax(axis=-1)
-
-  return result[0][0] == 1
-
 
 @app.post('/check-profanity')
 async def check_slang(item: Item):
-    
-   is_slang= predict(item.text)
-
-   return {
-     "success": True,
-     "code": 0,
-     "message": "string",
-     "data": {
-       "isSlang": is_slang 
-      }
-   }
-
+    is_slang = checker.predict(item.text)
+    return {
+        "success": True,
+        "code": 0,
+        "message": "string",
+        "data": {
+            "isSlang": is_slang
+        }
+    }
